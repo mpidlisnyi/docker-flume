9296dfdb935634f9dc210ea36ee1ec3261733a4e
Maksym Pidlisnyi <maksim@nightbook.info>
2017-04-27 18:22:35 +0300

    add remote configuration support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 FLUME_CONF=${FLUME_CONF:-$FLUME_CONF_DIR/flume.conf}
-MEMORY_SIZE=${MEMORY_SIZE:""}
 OPTS="-Dorg.apache.flume.log.printconfig=true -Dlog4j.configuration=file:${LOG4J_PROPERTIES}"
 if [ $MEMORY_SIZE ];
 then
         OPTS="${OPTS} -Xmx${MEMORY_SIZE}"
 fi
 
-#TODO add FLUME_CONF_URL for downloading configuration file remotely
-exec flume-ng agent -c ${FLUME_CONF_DIR} -n ${FLUME_AGENT_NAME} -f ${FLUME_CONF} ${OPTS} ${FLUME_OPTS:-$FLUME_CONF_DIR/flume.conf}
+if [ $FLUME_CONF_URL ];
+then
+        FLUME_REMOTE_CONF="/flume_remote.conf"
+        wget -qO ${FLUME_REMOTE_CONF} ${FLUME_CONF_URL}
+        OPTS="${OPTS} ${FLUME_REMOTE_CONF}"
+else
+        OPTS="${OPTS} ${FLUME_OPTS:-$FLUME_CONF_DIR/flume.conf}"
+fi
+
+exec flume-ng agent -c ${FLUME_CONF_DIR} -n ${FLUME_AGENT_NAME} -f ${FLUME_CONF} ${OPTS} ${@}
